e7edc612b82d7e01c2188c9afff6d10b5d00751b
David Razmadze <razd45@gmail.com>
2022-07-03 08:49:42 -0400

    Initial Commit

---
+++ TestPushNotifications/TestPushNotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestPushNotificationsApp.swift
+//  TestPushNotifications
+//
+//  Created by David Razmadze on 7/3/22.
+//
+
+import SwiftUI
+
+@main
+struct TestPushNotificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
