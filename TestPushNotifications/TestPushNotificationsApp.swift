891e4af81cd9b34281c1474b5250cfd7bbea10da
David Razmadze <razd45@gmail.com>
2022-07-03 08:52:45 -0400

    1. Create AppDelegate class

--- TestPushNotifications/TestPushNotificationsApp.swift
+++ TestPushNotifications/TestPushNotificationsApp.swift
@@ -9,9 +9,22 @@ import SwiftUI
 
 @main
 struct TestPushNotificationsApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
+}
+
+// MARK: - AppDelegate
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    print("Your code here")
+    return true
+  }
+  
 }
