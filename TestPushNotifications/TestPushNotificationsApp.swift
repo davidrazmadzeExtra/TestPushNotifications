6e6116601d9ad010b891ddfae43d68a1cb81fcb9
David Razmadze <razd45@gmail.com>
2022-07-03 08:54:17 -0400

    2. `registerForPushNotifications` - popup

--- TestPushNotifications/TestPushNotificationsApp.swift
+++ TestPushNotifications/TestPushNotificationsApp.swift
@@ -6,11 +6,12 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 @main
 struct TestPushNotificationsApp: App {
   @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-
+  
   var body: some Scene {
     WindowGroup {
       ContentView()
@@ -23,8 +24,18 @@ struct TestPushNotificationsApp: App {
 class AppDelegate: NSObject, UIApplicationDelegate {
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    print("Your code here")
+    
+    registerForPushNotifications()
     return true
+  }
+  
+  // MARK: - Notification Functions
+  
+  func registerForPushNotifications() {
+    UNUserNotificationCenter.current()
+      .requestAuthorization(options: [.alert, .sound, .badge]) {(granted, error) in
+        print("Permission granted: \(granted)")
+      }
   }
   
 }
